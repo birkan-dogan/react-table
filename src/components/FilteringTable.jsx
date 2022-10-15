56a0b90ede1c7f967c26aca74f4bbbd397bd4a16
birkan <b.561birkan@gmail.com>
2022-10-15 14:29:39 +0300

    Column filtering in react-table

--- src/components/FilteringTable.jsx
+++ src/components/FilteringTable.jsx
@@ -1,4 +1,4 @@
-import { useTable, useGlobalFilter } from "react-table";
+import { useTable, useGlobalFilter, useFilters } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS, GROUPED_COLUMNS } from "./columns";
 import { useMemo } from "react";
@@ -16,6 +16,7 @@ const FilteringTable = () => {
       columns,
       data,
     },
+    useFilters,
     useGlobalFilter
   );
 
@@ -46,7 +47,10 @@ const FilteringTable = () => {
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                <th {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                  <div>{column.canFilter && column.render("Filter")}</div>
+                </th>
               ))}
             </tr>
           ))}
